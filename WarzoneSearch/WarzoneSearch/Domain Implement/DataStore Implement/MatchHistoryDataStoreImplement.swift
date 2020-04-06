cbf10fa02b57369df07118c16cf97b7926c9ca74
GilwanRyu <oper0960@naver.com>
2020-04-06 17:26:23 +0900

    - - 검색기록이없는 계정 전적 못받아오는 문제 수정

--- WarzoneSearch/WarzoneSearch/Domain Implement/DataStore Implement/MatchHistoryDataStoreImplement.swift
+++ WarzoneSearch/WarzoneSearch/Domain Implement/DataStore Implement/MatchHistoryDataStoreImplement.swift
@@ -14,31 +14,44 @@ import Alamofire
 import SwiftyJSON
 
 class MatchHistoryDataStoreImplement: MatchHistoryDataStore {
-
+    
+    private var disposeBag = DisposeBag()
+    
     // 매치 히스토리
     func getUserMatchHistory(platform: Platform, id: String) -> Observable<MatchHistoryViewable> {
+        
+        let profileUrlString = Constants.MatchHistory.profile
+        
+        let replacingProfilePlatform = profileUrlString.replacingOccurrences(of: "platform", with: platform.matchHistoryName)
+        let replacingProfileid = replacingProfilePlatform.replacingOccurrences(of: "id", with: id.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)
+        
+        let profileObservable = RxAlamofire.requestData(.get, URL(string: replacingProfileid)!,
+                                                        parameters: [:],
+                                                        encoding: URLEncoding(destination: .methodDependent),
+                                                        headers: [:])
+            .map { _ , data in
+                return JSON(data)
+        }
         
         let urlString = Constants.MatchHistory.matchHistory
         
         let replacingPlatform = urlString.replacingOccurrences(of: "platform", with: platform.matchHistoryName)
         let replacingid = replacingPlatform.replacingOccurrences(of: "id", with: id.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)
         
-        print(replacingid)
-        
-        return RxAlamofire.requestData(.get, URL(string: replacingid)!,
-                                       parameters: [:],
-                                       encoding: URLEncoding(destination: .methodDependent),
-                                       headers: [:])
-            .map { _ , data in
-                
-                
-                print(JSON(data))
-                
-                let decodableJson = try JSONDecoder().decode(MatchHistoryCodable.self, from: data)
+        let historyObservable = RxAlamofire.requestData(.get, URL(string: replacingid)!,
+                                                        parameters: nil,
+                                                        encoding: URLEncoding(destination: .methodDependent),
+                                                        headers: nil)
+            .map { data -> MatchHistoryViewable in
+                let decodableJson = try JSONDecoder().decode(MatchHistoryCodable.self, from: data.1)
                 
                 guard let data = decodableJson.data else { return MatchHistoryViewModel() }
                 
                 return MatchHistoryViewModel(matchHistoryData: data)
         }
+        
+        return profileObservable
+            .filter{ profile in profile["data"]["platformInfo"]["platformUserHandle"].string != nil}
+            .concatMap({ profile in historyObservable })
     }
 }
